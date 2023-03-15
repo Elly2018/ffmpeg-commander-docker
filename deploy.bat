e4c14eb414c7ed3f156388b104ed18a095b827a7
ZhuElly <34325707+Elly2018@users.noreply.github.com>
2023-03-15 15:31:16 +0800

    update deploy processes

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,3 @@
 @echo off
-docker run -d --name ffmpeg_ui --network=funique --ip 10.0.3.4 e87870823/web_backend_view:latest
+docker run -d --name ffmpeg_ui --network=funique --ip 10.0.3.4 e87870823/ffmpeg_ui:latest
 pause
